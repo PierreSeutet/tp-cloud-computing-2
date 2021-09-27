f7104aa23720ad275ea1c90da77ceaf773170f7c
PierreSeutet <60437955+PierreSeutet@users.noreply.github.com>
2021-09-27 14:50:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 import os
-from flask import Flask, jsonify, request
-from transformers import pipeline
+
+from flask import Flask
 
 app = Flask(__name__)
 
-model_path = "./model"
+@app.route("/")
+def hello_world():
+    name = os.environ.get("NAME", "World")
+    return "Hello {}!".format(name)
 
-@app.route('/run')
-def classify():
-  # Your function
-  # classify = pipeline("sentiment-analysis", model=model_path, tokenizer=model_path)
-  
-if __name__ == '__main__':
-    # This is used when running locally only. When deploying to Google Cloud
-    # Run, a webserver process such as Gunicorn will serve the app.
-    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
